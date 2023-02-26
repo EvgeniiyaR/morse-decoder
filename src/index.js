147a2d574b90227885a39944cad00ad9c606cc7c
EvgeniiyaR <tulpansik@mail.ru>
2023-02-26 23:22:48 +0300

    init: complete decoder

--- src/index.js
+++ src/index.js
@@ -38,9 +38,49 @@ const MORSE_TABLE = {
 };
 
 function decode(expr) {
-    // write your solution here
+    let newExp = expr.split('**********');
+    let a = [];
+    for (let i = 0; i < newExp.length; i++) {
+        let count = 0;
+        let c = [];
+        let b = [];
+        for (let j of newExp[i]) {
+            b.push(j);
+            count += 1;
+            if (count === 10) {
+                c.push(b.join(''));
+                b = [];
+                count = 0;
+            }
+        }
+        b = [];
+        for (let i = 0; i < c.length; i++) {
+            let count = 0;
+            let g = [];
+            let h = [];
+            for (let j of c[i]) {
+                g.push(j);
+                count += 1;
+                if (count === 2) {
+                    if (g.join('') == '10') {
+                        h.push('.');
+                    } else if (g.join('') == '11') {
+                        h.push('-');
+                    }
+                    g = [];
+                    count = 0;
+                }
+            }
+            b.push(MORSE_TABLE[h.join('')]);
+        }
+        a.push(b.join(''));
+    }
+    return a.join(" ");
 }
+
+decode(expr);
 
 module.exports = {
     decode
 }
+
